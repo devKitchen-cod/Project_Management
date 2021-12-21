661edc4473e8a5acbec4d146503ef5451aea8e0e
Sergey <57343283+devKitchen-cod@users.noreply.github.com>
2021-12-21 15:27:50 +0200

    Add files via upload

---
+++ Tracker_Front/src/Component/vueProject/CreateTask.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function CreateTask(){
+
+  return(
+    <div>create task</div>
+  )
+}
---
+++ Tracker_Front/src/Component/vueProject/Tasks.js
@@ -0,0 +1,49 @@
+
+import React, { useEffect } from 'react'
+import { Button, Table } from 'react-bootstrap'
+import { useDispatch } from 'react-redux'
+
+export default function Tasks(){
+
+  const dispatch = useDispatch();
+ // useEffect(() =>{dispatch()}, [])
+
+  return(
+    <div>
+ 
+    <Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Task Name</th>
+      <th>Developer</th>
+      <th>Time for task</th>
+      
+    </tr>
+  </thead>
+
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Task 1</td>
+      <td>Otto</td>
+      <td>12    <Button>Some</Button></td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Task 2</td>
+      <td>Thornton</td>
+      <td>5</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>Task 3</td>
+      <td>Devid</td>
+      <td>10</td>
+      
+    </tr>
+  </tbody>
+</Table>
+</div>
+  )
+}
